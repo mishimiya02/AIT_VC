8e177742b365da285734b5884f44a0720121ead5
sandy <mishimiya02@gmail.com>
2026-01-08 10:55:25 -0600

    cxcx

--- java/loginprincipal.js
+++ java/loginprincipal.js
@@ -2,7 +2,7 @@
       const pass = document.getElementById("password").value.trim();
       const error = document.getElementById("error");
 
-      const contrasenasValidas = ["RMV", "JM", "ADMAIT"];
+      const contrasenasValidas = ["RMV", "JM", "ADMAIT","CXC"];
 
       if (contrasenasValidas.includes(pass)) {
         window.location.href = destino;
